96a781c65bc115b5d0e207e866b814e4a96a5774
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-09 06:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function divide(a,b){
   return a / b
 }
 function inc(n){
-  n++
+  return n++
 }
 function dec(n){
-  n--
+  return n--
 }
 function makeInt(n){
-  parseInt(n,10);
+  return parseInt(n,10);
 }
 function preserveDecimal(n){
-  parseFloat(n);
+  return parseFloat(n);
 }
